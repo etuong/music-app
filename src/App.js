4805645835e0fd3575c2df5be93fbe323788e453
etuong <etuong@gmail.com>
2022-11-12 12:26:56 -0800

    Before changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styled2 from "@emotion/styled";
-import { styled, useTheme } from '@mui/material/styles';
+import { styled, useTheme } from "@mui/material/styles";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -15,19 +15,20 @@ const MainContainer = styled2.div`
   height: 100vh;
 `;
 
-
-
 const Widget = styled("div")(({ theme }) => ({
-  backgroundColor:"rgba(255,255,255,0.4)",
+  backgroundColor:
+    theme.palette.mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.4)",
   backdropFilter: "blur(40px)",
 }));
 
 function App() {
   return (
     <MainContainer>
-<Widget>
-   </Widget>
-  
+      <Playlist />
+      <Stack sx={{ width: "100%" }}>
+        <Controls />
+        <Songs />
+      </Stack>
     </MainContainer>
   );
 }
